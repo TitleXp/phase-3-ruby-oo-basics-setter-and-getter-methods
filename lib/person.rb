06db96103409029dd5f677ec8250f4a741de3389
Surapat Mekvanich <titleyugio@gmail.com>
2023-01-10 21:54:15 -0800

    commit tree 9d5da4c78122d9aa99178c1862c488aaf5e65d0c

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    def name=(name) #setter fxn
+        @name = name
+    end
+
+    def name #getter fxn
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
 end
+
+brennan = Person.new
+brennan.name=("Brennan")
+brennan.name
